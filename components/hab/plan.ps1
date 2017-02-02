b116c355a7af74c7913058efdda0878787f71ecc
Matt Wrock <matt@mattwrock.com>
2017-02-02 07:32:41 -0800

    a windows plan for habitat
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- components/hab/plan.ps1
+++ components/hab/plan.ps1
@@ -3,38 +3,40 @@ $pkg_origin = "core"
 $pkg_version = "$(Get-Content $PLAN_CONTEXT/../../VERSION)"
 $pkg_maintainer = "The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license = @("Apache-2.0")
-$pkg_source = "nosuchfile.tar.gz"
+$pkg_source = "https://s3-us-west-2.amazonaws.com/habitat-win-deps/hab-win-deps.zip"
+$pkg_shasum="1a42a5597843303eac8c31fcafd809dd618a92b69eaea8a51f577c71c7b2fd00"
 $pkg_bin_dirs = @("bin")
-
-# No runtime or build dependencies yet
-$pkg_deps = @()
-$pkg_build_deps = @()
-
-function _Invoke-CommonPrepare {
-    Invoke-DefaultPrepare
-
-    # Can be either `--release` or `--debug` to determine cargo build strategy
-    $script:build_type = "--release"
-    Write-BuildLine "Building artifacts with $($build_type.Substring(2))"
-
-    # Used by the `build.rs` program to set the version of the binaries
-    $env:PLAN_VERSION = "${pkg_version}/${pkg_release}"
-    Write-BuildLine "Setting PLAN_VERSION=$env:PLAN_VERSION"
-
-    # Used by Cargo to use a pristine, isolated directory for all compilation
-    $env:CARGO_TARGET_DIR = "$HAB_CACHE_SRC_PATH\$pkg_dirname"
-    Write-BuildLine "Setting CARGO_TARGET_DIR=$env:CARGO_TARGET_DIR"
-}
+$pkg_build_deps = @("core/rust")
 
 function Invoke-Prepare {
-    _Invoke-CommonPrepare
+    $env:PLAN_VERSION               = "$pkg_version/$pkg_release"
+    $env:CARGO_TARGET_DIR           = "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+    $env:LIB                        = "$HAB_CACHE_SRC_PATH/$pkg_dirname/lib"
+    $env:INCLUDE                    = "$HAB_CACHE_SRC_PATH/$pkg_dirname/include"
+    $env:SODIUM_LIB_DIR             = "$HAB_CACHE_SRC_PATH/$pkg_dirname/lib"
+    $env:LIBARCHIVE_INCLUDE_DIR     = "$HAB_CACHE_SRC_PATH/$pkg_dirname/include"
+    $env:LIBARCHIVE_LIB_DIR         = "$HAB_CACHE_SRC_PATH/$pkg_dirname/lib"
+    $env:OPENSSL_LIBS               = 'ssleay32:libeay32'
+    $env:OPENSSL_LIB_DIR            = "$HAB_CACHE_SRC_PATH/$pkg_dirname/lib"
+    $env:OPENSSL_INCLUDE_DIR        = "$HAB_CACHE_SRC_PATH/$pkg_dirname/include"
+}
 
-    $script:rustc_target = "x86_64-pc-windows-gnu"
-    Write-BuildLine "Setting rustc_target=$rustc_target"
+function Invoke-Unpack {
+  Expand-Archive -Path "$HAB_CACHE_SRC_PATH/hab-win-deps.zip" -DestinationPath "$HAB_CACHE_SRC_PATH/$pkg_dirname"
 }
 
 function Invoke-Build {
     Push-Location "$PLAN_CONTEXT"
-#    cargo build $build_type.Replace("--debug", "") --target=$rustc_target --verbose
-    Pop-Location
+    try {
+        cargo build --release
+        if($LASTEXITCODE -ne 0) {
+            Write-Error "Cargo build failed!"
+        }
+    }
+    finally { Pop-Location }
+}
+
+function Invoke-Install {
+    Copy-Item "$env:CARGO_TARGET_DIR/release/hab.exe" "$pkg_prefix/bin/hab.exe"
+    Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/bin/*" "$pkg_prefix/bin"
 }
